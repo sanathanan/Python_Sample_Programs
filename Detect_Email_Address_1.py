174cbd1309cd6de1f91391bcae81559fa5612bdf
V.T. Sanathanan <75120579+sanathanan@users.noreply.github.com>
2021-06-22 12:49:44 +0530

    Add files via upload

--- Detect_Email_Address_1.py
+++ Detect_Email_Address_1.py
@@ -1,11 +1,12 @@
 import re
 
 def email_identification(sent):
-    email_identified = re.findall('\S+@\S+', sent)
+    email_identified = re.findall('[\w\-\.]+[@]+[a-z]+[\.]+[a-z]{2,3}', sent)
     return email_identified
 
 
-sent = input(" ")
+
+sent = input("Enter the string:  ")
 result = email_identification(sent)
 
 print(result)
